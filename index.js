60cae5c731553d160031812385b7adc4ad67884b
juliojesus15 <juliojesus1508@gmail.com>
2022-05-31 23:22:00 -0500

    try puppeteer

--- index.js
+++ index.js
@@ -6,48 +6,40 @@ const app = express()
 // settings
 app.set('port', process.env.PORT || 4000);
 
-
+// Routes
 app.get('/', (req, res) => {
-  res.send('Rubik Cube Solver - API')
+  res.send("Rubik's Cube Solver - API")
 })
 
-app.get('/solver/:keys', async (req, res) => {  
-  //https://rubikscu.be/solver/?cube=0656612513251322441454634341112543236523251413235666645
-  //656612513251322441454634341112543236523251413235666645
-  const keysParams = req.params.keys
-  url = `https://rubikscu.be/solver/?cube=0${keysParams}`
-  console.log(url)
-  const browser = await puppeteer.launch()
+app.get('/solver/:keys', async (req, res) => {      
+  const url = `https://rubikscu.be/solver/?cube=0${req.params.keys}`
+  
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox"]
+  })
   const page = await browser.newPage()
 
   await page.goto(url)
-  //await page.screenshot({path: 'test1.jpg'})
-
-  //Wait for thisRotIs0  
   await page.waitForSelector('[class=thisRotIs0]')
-  //await page.screenshot({path: 'test2.jpg'})
-
-  //to get segedvaltozo
+  
   const solver = await page.evaluate( () => {
     const elements = document.querySelectorAll('[id=segedvaltozo] span')
-
-    const steps = []
-    for(let element of elements) {
-      content = element.textContent
-      step = content.replace("'", "+");
-      steps.push(step)
-    }
-    return steps;
-
-  })  
-  solver.shift()
     
+    const steps = []            
+    for(let element of elements) {      
+      steps.push(element.textContent.replace("'", "+"))
+    }
+    return steps;    
+  })  
+
+  solver.shift()    
   await browser.close()
 
-  res.send(JSON.stringify({ 'Solver': solver }))
+  res.send(JSON.stringify({ 'steps': solver }))
 })
 
-// starting the server
+// Server
 app.listen(app.get('port'), () => {
   console.log(`Server on port ${app.get('port')}`)
 })
